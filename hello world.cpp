6e85a27a3d1003203dd2b39503c7b405f04ead3c
constant-variable <56024530+constant-variable@users.noreply.github.com>
2019-10-01 15:37:44 +0530

    create hello world.cpp
    Adding file for hactoberfest

---
+++ hello world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //Import library
+
+using namespace std; //Declaring syntax
+
+int main() {    //Driver Code
+
+cout << "Hello World!";     //Main code which prints Hello World!
+
+}
